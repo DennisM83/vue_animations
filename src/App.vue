5cdc0f459e209393556c7db90d9b7e365f4b1dd9
Dennis Wenrich <dennisfinley@gmail.com>
2022-03-30 19:31:54 -0400

    added animation api for zoom effect

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,21 @@
 <!--    <h2 v-if="flag" key="main">Hello World!</h2>-->
 <!--    <h2 v-else key="secondary">Another hello!</h2>-->
 <!--  </transition>-->
-  <transition>
-    <h2>Hello</h2>
+<!--  <transition name="zoom" appear>-->
+<!--    <h2 v-if="flag">Hello</h2>-->
+<!--  </transition>-->
+  <transition
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+    :css="false"
+  >
+    <h2 v-if="flag">Hey</h2>
   </transition>
+
 </template>
 
 <script>
@@ -16,13 +28,53 @@ export default {
   name: 'App',
   data() {
     return {
-      flag: false
+      flag: true,
+    }
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log('before-entered event fired', el)
+    },
+    enter(el, done) {
+      console.log('entered event fired', el);
+
+      const animation = el.animate([{ transform: "scale3d(0,0,0)"}, {}], {
+        duration: 1000,
+      })
+
+      animation.onfinish = () => {
+        done();
+      }
+    },
+    afterEnter(el) {
+      console.log('after-entered event fired', el)
+    },
+    beforeLeave(el) {
+      console.log('before-leave event fired', el)
+    },
+    leave(el, done) {
+      console.log('leave event fired', el);
+      const animation = el.animate([{}, { transform: "scale3d(0,0,0)"}], {
+        duration: 1000,
+      })
+
+      animation.onfinish = () => {
+        done();
+      }
+    },
+    afterLeave(el) {
+      console.log('after-leave event fired', el)
     }
   }
 }
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
 .fade-enter-from  {
   opacity: 0;
 }
@@ -34,6 +86,43 @@ export default {
 .fade-leave-to {
   transition: all 0.5s linear;
   opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 0.5s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 0.5s linear;
+}
+
+.zoom-enter-from  {
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  opacity: 0;
+}
+
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 
 </style>

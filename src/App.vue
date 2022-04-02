6d1779e0bb6aa7da3454427ccbfe9fd71e56d2ae
Dennis Wenrich <dennisfinley@gmail.com>
2022-04-02 15:15:49 -0400

    built out an add and remove function for numbers

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,26 @@
 <!--  <transition name="zoom" appear>-->
 <!--    <h2 v-if="flag">Hello</h2>-->
 <!--  </transition>-->
-  <transition
-    @before-enter="beforeEnter"
-    @enter="enter"
-    @after-enter="afterEnter"
-    @before-leave="beforeLeave"
-    @leave="leave"
-    @after-leave="afterLeave"
-    :css="false"
-  >
-    <h2 v-if="flag">Hey</h2>
-  </transition>
+<!--  <transition-->
+<!--    @before-enter="beforeEnter"-->
+<!--    @enter="enter"-->
+<!--    @after-enter="afterEnter"-->
+<!--    @before-leave="beforeLeave"-->
+<!--    @leave="leave"-->
+<!--    @after-leave="afterLeave"-->
+<!--    :css="true"-->
+<!--    name="fade"-->
+<!--  >-->
+<!--    <h2 v-if="flag">Hey</h2>-->
+<!--  </transition>-->
+
+  <button @click="addItem">Add</button>
+  <ul>
+    <li v-for="(number, index) in numbers" :key="number" @click="removeItem(index)">
+      {{ number }}
+    </li>
+  </ul>
+
 
 </template>
 
@@ -29,22 +38,32 @@ export default {
   data() {
     return {
       flag: true,
+      numbers: [1, 2, 3, 4, 5],
     }
   },
   methods: {
+    addItem() {
+      const num = Math.floor(Math.random() * 100 + 1);
+      const index = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(index, 0, num);
+    },
+    removeItem(index) {
+      this.numbers.splice(index, 1);
+    },
+
     beforeEnter(el) {
       console.log('before-entered event fired', el)
     },
-    enter(el, done) {
+    enter(el) {
       console.log('entered event fired', el);
 
-      const animation = el.animate([{ transform: "scale3d(0,0,0)"}, {}], {
-        duration: 1000,
-      })
-
-      animation.onfinish = () => {
-        done();
-      }
+      // const animation = el.animate([{ transform: "scale3d(0,0,0)"}, {}], {
+      //   duration: 1000,
+      // })
+      //
+      // animation.onfinish = () => {
+      //   done();
+      // }
     },
     afterEnter(el) {
       console.log('after-entered event fired', el)
@@ -52,15 +71,15 @@ export default {
     beforeLeave(el) {
       console.log('before-leave event fired', el)
     },
-    leave(el, done) {
+    leave(el) {
       console.log('leave event fired', el);
-      const animation = el.animate([{}, { transform: "scale3d(0,0,0)"}], {
-        duration: 1000,
-      })
-
-      animation.onfinish = () => {
-        done();
-      }
+      // const animation = el.animate([{}, { transform: "scale3d(0,0,0)"}], {
+      //   duration: 1000,
+      // })
+      //
+      // animation.onfinish = () => {
+      //   done();
+      // }
     },
     afterLeave(el) {
       console.log('after-leave event fired', el)
@@ -70,6 +89,10 @@ export default {
 </script>
 
 <style>
+li {
+  font-size: 22px;
+  cursor: pointer;
+}
 h2 {
   width: 400px;
   padding: 20px;
